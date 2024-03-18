6d5757a358b2731927ce07d4358f1accde949af2
promosirupiah <promosirupiah@gmail.com>
2024-03-19 04:48:29 +0700

    Signed-off-by: promosirupiah <promosirupiah@gmail.com>

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$environment_variable = '{"themes":{"1":{"themes_url":"https:\/\/madxartwork.eu.org\/wp-content\/themes","use_child_theme":false,"main":{"folder_name":"madxflow","mapped_name":""}}},"allowed_paths":["D:\/xampp\/htdocs\/wp-content\/themes"],"wordpress_directory":"","site_relative_path":"\/"}'; ?>
